6317b02824d5240161bc4273c5df16e7b1c0bfc8
max-borisov <max.borisov@yahoo.com>
2015-06-26 14:28:00 +0300

    Checkout and orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @orders = current_user.orders.includes(:order_items).order(created_at: :desc)
   end
 
   def create
+    Order.create!(
+      user_id: current_user.id,
+      total_price: current_user.shopping_cart_total_price
+    )
+    redirect_to orders_path
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,17 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items, dependent: :destroy
+  after_save :create_items
 
   validates :user_id, :total_price, presence: true
+
+  private
+    def create_items
+      user = User.find(user_id)
+      shopping_cart_items = user.shopping_cart_items.pluck(:book_id)
+      res = []
+      shopping_cart_items.each { |book_id| res.push({ book_id: book_id, order_id: id }) }
+      OrderItem.create!(res)
+      user.shopping_cart_items.destroy_all
+    end
 end
