dd2074669a9602cb0b15898a17a9ecd7748c7109
max-borisov <max.borisov@yahoo.com>
2015-06-26 15:23:39 +0300

    CRUD for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,25 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  
   def index
     @books = Book.order(created_at: :asc).all
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
     @reviews = @book.reviews
     @review = Review.new
   end
@@ -21,8 +28,25 @@ class BooksController < ApplicationController
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    title = @book.title
+    @book.destroy
+    redirect_to books_path, notice: "Book \"#{title}\" has been deleted"
   end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :publisher, :pub_date, :description, :price, :isbn, :amazon_id)
+    end
 end
