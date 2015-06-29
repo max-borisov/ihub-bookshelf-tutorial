e57f30ad21e52d2d55c8f3b1e8f3df18b1485767
max-borisov <max.borisov@yahoo.com>
2015-06-29 16:54:45 +0300

    User profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_access
+  before_action :set_user
+
   def show
   end
 
   def destroy
+    @user.destroy
+    redirect_to new_user_registration_path, notice: "User account has been deleted"
   end
+
+  private
+    def check_access
+      redirect_to new_user_session_path unless params[:id] == current_user.id.to_s
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
